d6317f47b1ac0dba766e4347b19c97f3e721fcc9
“mishealcodile” <ezekielmisheal4@gmail.com>
2020-04-26 01:14:57 +0100

    fixed page not found on profile loading

--- client/src/components/notFound/notFound.js
+++ client/src/components/notFound/notFound.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const NotFound =()=> {
+export default () =>{
     return (
         <div>
             <h1 className="display-4">Page Not Found</h1>
@@ -8,5 +8,3 @@ const NotFound =()=> {
         </div>
     )
 }
-
-export default NotFound
